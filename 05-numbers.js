fe556ee779fccd086b1c32a66af751ca35568f09
JaviGuz <javier.guzman@feverup.com>
2019-06-12 15:31:04 +0200

    Boolean operations

--- 05-numbers.js
+++ 05-numbers.js
@@ -3,6 +3,26 @@ let gravity = 9.81;
 let PI = 3.14;
 let temperature = 37;
 const boilingPoint = 100;
-console.log(age);
-console.log(gravity);
-console.log(PI);
+const bodyTemp = 37; 
+console.log(age, gravity, PI, boilingPoint, bodyTemp);
+
+//Two statements which provide truthy value
+console.log(age > gravity);
+console.log(temperature < (gravity * age));
+
+
+//Operations that devolve boolean
+console.log ("4 > 3 : ", 4 > 3);
+console.log ("4 >= 3 : ", 4 >= 3);
+console.log ("4 < 3 : ", 4 < 3);
+console.log ("4 <= 3 : ", 4 <= 3);
+console.log ("4 == 4 : ", 4 == 4);
+console.log ("4 === 4 : ", 4 === 4);
+console.log ("4 != 4 : ", 4 != 4);
+console.log ("4 !== 4 : ", 4 !== 4);
+console.log ("4 != '4' : ", 4 != '4');
+console.log ("4 == '4' : ", 4 == '4');
+console.log ("4 === '4' : ", 4 === '4');
+
+let firstName;
+console.log(firstName)
